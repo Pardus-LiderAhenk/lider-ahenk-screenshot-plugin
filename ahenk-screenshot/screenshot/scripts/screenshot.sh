3b8642659af559ce8d9da562350bbde0eff3b250
Volkan Şahin <volkan.sahin@agem.com.tr>
2016-04-13 23:01:54 +0300

    bug fixes

--- ahenk-screenshot/screenshot/scripts/screenshot.sh
+++ ahenk-screenshot/screenshot/scripts/screenshot.sh
@@ -4,7 +4,7 @@ if [ $# -lt 1 ]; then
 	echo "Example: screenshot.sh /tmp/scr.png 0"
 	exit 1;
 fi
-display=0;
+display="0";
 if [ $# -gt 1 ]; then
 	display=$2
 fi
@@ -12,9 +12,10 @@ if ! which convert > /dev/null; then
 	apt-get install -y imagemagick
 fi
 w -oush | grep tty | awk '{print $1, $3}' | while read x; do 
+	echo $x > /tmp/oner.txt
 	array=(${x//:/ })
 	if [ $display -eq ${array[1]} ]; then
 		echo "Screenshot username:${array[0]} display:${array[1]}"
-		su - ${array[0]} -c "xwd -root -display :${array[1]} | convert - png:- > $1"
+		su - ${array[0]} -c "xwd -root -display :${array[1]} | convert -resize 30% - jpg:- > $1"
 	fi
 done
--- ahenk-screenshot/screenshot/take-screenshot.py
+++ ahenk-screenshot/screenshot/take-screenshot.py
@@ -3,50 +3,52 @@
 # Author: Mine DOGAN <mine.dogan@agem.com.tr>
 
 
+import datetime
 from base.model.ContentType import ContentType
 from base.model.MessageCode import MessageCode
-from base.model.MessageType import MessageType
 from base.plugin.AbstractCommand import AbstractCommand
 
+
 class TakeScreenshot(AbstractCommand):
-    
     def __init__(self, task, context):
         super(TakeScreenshot, self).__init__()
         self.task = task
         self.context = context
-        
-        self.screenshot = self.get_screenshot()
+        self.shot_path = self.get_shot_path()
 
-        self.install_scrot = 'apt-get -y install scrot'
-        self.take_screenshot = 'scrot '+self.screenshot
-        
+        self.take_screenshot = '/bin/bash ./plugins/screenshot/scripts/screenshot.sh ' + self.shot_path
+
     def handle_task(self):
-#        process = self.context.execute(self.install_scrot)
-#        process.wait()
-        
-#        process = self.context.execute(self.take_screenshot)
-#        process.wait()
-        md5sum = self.scope.getExecutionManager().get_md5_file(self.screenshot)
-        self.scope.getMessager().send_file(self.screenshot)
 
-        data={'md5':md5sum}
+        process = self.context.execute(self.take_screenshot)
+        process.wait()
 
-        self.create_response(message='_message', data=data, content_type=ContentType.IMAGE_PNG.value)
+        md5sum = self.scope.getExecutionManager().get_md5_file(str(self.shot_path))
+        print('md5:' + md5sum)
 
-    def get_screenshot(self):
-        return '/tmp/oner.png'
+        try:
+            self.scope.getMessager().send_file(self.shot_path)
+        except Exception as e:
+            print('--->' + str(e))
+
+        data = {'md5': md5sum}
+
+        self.create_response(message='_message', data=data, content_type=ContentType.IMAGE_JPEG.value)
+
+    def get_shot_path(self):
+        return '/tmp/ahenk_screenshot_' + str(datetime.datetime.now().strftime("%d%m%Y%I%M")) + '.jpg'
 
     def create_response(self, success=True, message=None, data=None, content_type=None):
-       if success:
-           self.context.put('responseCode', MessageCode.TASK_PROCESSED.value)
-       else:
-           self.context.put('responseCode', MessageCode.TASK_ERROR.value)
-       self.context.put('responseMessage', message)
-       self.context.put('responseData', data)
-       self.context.put('contentType', content_type)
+        if success:
+            self.context.put('responseCode', MessageCode.TASK_PROCESSED.value)
+        else:
+            self.context.put('responseCode', MessageCode.TASK_ERROR.value)
+        self.context.put('responseMessage', message)
+        self.context.put('responseData', data)
+        self.context.put('contentType', content_type)
+
 
 def handle_task(task, context):
-    
+    print('TAKE SHOT')
     screenshot = TakeScreenshot(task, context)
     screenshot.handle_task()
-
