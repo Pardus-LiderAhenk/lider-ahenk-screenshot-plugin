ec40054fc52014077f1ed90a0abeaa4c77271acc
Volkan Şahin <bm.volkansahin@gmail.com>
2016-10-07 11:08:52 +0300

    log fixing

--- ahenk-screenshot/screenshot/take-screenshot.py
+++ ahenk-screenshot/screenshot/take-screenshot.py
@@ -30,7 +30,7 @@ class TakeScreenshot(AbstractPlugin):
                 user_name = self.data[self.Ahenk.dn()]
 
             if not user_name:
-                self.logger.debug('[SCREENSHOT] Taking screenshot with default display.')
+                self.logger.debug('Taking screenshot with default display.')
                 self.execute(self.take_screenshot)
 
             else:
@@ -40,50 +40,50 @@ class TakeScreenshot(AbstractPlugin):
 
                 ##permission
                 self.logger.debug(
-                    '[SCREENSHOT] Asking for screenshot to user {0} on {1} display'.format(user_name, user_display))
+                    'Asking for screenshot to user {0} on {1} display'.format(user_name, user_display))
 
                 user_answer = self.ask_permission(user_display, user_name,
                                                   "Ekran görüntüsünün alınmasına izin veriyor musunuz?",
                                                   "Ekran Görüntüsü")
 
                 if user_answer is None:
-                    self.logger.error('[SCREENSHOT] User answer could not keep.')
+                    self.logger.error('User answer could not keep.')
                     self.context.create_response(code=self.message_code.TASK_ERROR.value,
                                                  message='Ekran görüntüsü alırken hata oluştu: Kullanıcı iznine erişilemedi.')
                     return
 
                 elif user_answer is True:
-                    self.logger.debug('[SCREENSHOT] User accepted for screenshot')
-                    self.logger.debug('[SCREENSHOT] Taking screenshot with specified display: {0}'.format(user_display))
+                    self.logger.debug('User accepted for screenshot')
+                    self.logger.debug('Taking screenshot with specified display: {0}'.format(user_display))
                     self.execute(self.take_screenshot + ' ' + user_display.replace(':', ''))
-                    self.logger.debug('[SCREENSHOT] Screenshot command executed.')
+                    self.logger.debug('Screenshot command executed.')
                 else:
-                    self.logger.warning('[SCREENSHOT] User decline to screenshot.')
+                    self.logger.warning('User decline to screenshot.')
                     self.context.create_response(code=self.message_code.TASK_WARNING.value,
                                                  message='Eklenti başarıyla çalıştı fakat; kullanıcı ekran görüntüsü alınmasına izin vermedi.')
                     return
                 ##permission###
 
             if self.is_exist(self.shot_path):
-                self.logger.debug('[SCREENSHOT] Screenshot file found.')
+                self.logger.debug('Screenshot file found.')
 
                 data = {}
                 md5sum = self.get_md5_file(str(self.shot_path))
-                self.logger.debug('[SCREENSHOT] {0} renaming to {1}'.format(self.temp_file_name, md5sum))
+                self.logger.debug('{0} renaming to {1}'.format(self.temp_file_name, md5sum))
                 self.rename_file(self.shot_path, self.Ahenk.received_dir_path() + md5sum)
-                self.logger.debug('[SCREENSHOT] Renamed.')
+                self.logger.debug('Renamed.')
                 data['md5'] = md5sum
                 self.context.create_response(code=self.message_code.TASK_PROCESSED.value,
                                              message='Ekran görüntüsü başarıyla alındı.',
                                              data=json.dumps(data),
                                              content_type=self.get_content_type().IMAGE_JPEG.value)
-                self.logger.debug('[SCREENSHOT] SCREENSHOT task is handled successfully')
+                self.logger.debug('SCREENSHOT task is handled successfully')
             else:
                 raise Exception('Image not found this path: {}'.format(self.shot_path))
 
         except Exception as e:
             self.logger.error(
-                '[SCREENSHOT] A problem occured while handling SCREENSHOT task: {0}'.format(traceback.format_exc()))
+                'A problem occured while handling SCREENSHOT task: {0}'.format(traceback.format_exc()))
             self.context.create_response(code=self.message_code.TASK_ERROR.value,
                                          message='Ekran görüntüsü alırken hata oluştu: {0}'.format(str(e)))
 
